be5fdf763412ef670d873d8be5ef8b1c475f5b22
krook1024 <krook1024@gmail.com>
2019-06-13 14:01:08 +0200

    segédszkriptek újragondolva.

--- util/gen_example.sh
+++ util/gen_example.sh
@@ -1,9 +1,25 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-for i in $(seq 1 5)
-do
-	python3 generate.py
-done
+cdir="."
 
-rm -f doc/example/*.pdf
-mv ./*.pdf doc/example
+gen_example() {
+	for i in $(seq 1 5)
+	do
+		python3 $cdir/generate.py
+	done
+}
+
+delete_examples() {
+	rm -f $cdir/doc/example/*.pdf
+}
+
+move_examples() {
+	mv $cdir/*.pdf $cdir/doc/example
+}
+
+# Amennyiben az util mappából hívjuk a szkriptet, úgy a szülő mappába szükséges átlépnünk a futtatás előtt.
+[[ "$PWD" =~ util ]] && cd ".."
+
+gen_example
+delete_examples
+move_examples
--- util/sort_q.sh
+++ util/sort_q.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-sort < ../q.txt | uniq > tmp.txt
-rm -f '../q.txt'
-mv tmp.txt ../q.txt
+[[ "$PWD" =~ util ]] && cd ..
+
+sort < ./q.txt | uniq > tmp.txt
+rm -f ./q.txt
+mv tmp.txt ./q.txt
 rm -f tmp.txt
