27c0d9b8f9fc57dd31d8ea94e4e3583a1c3df13b
krook1024 <krook1024@gmail.com>
2019-06-13 14:02:20 +0200

    szkriptekhez BASH kell (=~ operátor végett)

--- util/gen_example.sh
+++ util/gen_example.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-cdir="."
-
 gen_example() {
 	for i in $(seq 1 5)
 	do
-		python3 $cdir/generate.py
+		python3 ./generate.py
 	done
 }
 
 delete_examples() {
-	rm -f $cdir/doc/example/*.pdf
+	rm -f ./doc/example/*.pdf
 }
 
 move_examples() {
-	mv $cdir/*.pdf $cdir/doc/example
+	mv ./*.pdf ./doc/example
 }
 
 # Amennyiben az util mappából hívjuk a szkriptet, úgy a szülő mappába szükséges átlépnünk a futtatás előtt.
