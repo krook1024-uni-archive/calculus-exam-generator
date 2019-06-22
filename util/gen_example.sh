84bedfb4feb58b07716fb3efe6c1a82e88f29027
krook1024 <krook1024@gmail.com>
2019-06-22 15:47:10 +0200

    példák fájlnevei normalizálva automatikusan, hibás kérdések javítva, új példák

--- util/gen_example.sh
+++ util/gen_example.sh
@@ -15,9 +15,19 @@ move_examples() {
 	mv ./*.pdf ./doc/example
 }
 
+rename_examples() {
+	i=1
+	workdir=./doc/example
+	for f in `ls $workdir`; do
+		mv $workdir/$f $workdir/$i.pdf
+		i=$(($i+1))
+	done
+}
+
 # Amennyiben az util mappából hívjuk a szkriptet, úgy a szülő mappába szükséges átlépnünk a futtatás előtt.
 [[ "$PWD" =~ util ]] && cd ".."
 
 gen_example
 delete_examples
 move_examples
+rename_examples
